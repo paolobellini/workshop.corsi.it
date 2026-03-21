6650d9876d06087ab4cb0366d96b290f56fb82a1
Paolo Bellini <paolobellini.web@gmail.com>
2026-03-21 23:22:59 +0100

    wip: create waiting list migration

---
+++ database/migrations/2026_03_21_222130_create_workshop_waiting_lists_table.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('workshop_waiting_lists', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('workshop_id')->constrained();
+            $table->foreignId('user_id')->constrained();
+            $table->timestamps();
+
+            $table->unique(['workshop_id', 'user_id']);
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('workshop_waiting_lists');
+    }
+};
