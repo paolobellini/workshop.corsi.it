631bb8a1856baf42fc674aa8b70d5b04a4256bbe
Paolo Bellini <paolobellini.web@gmail.com>
2026-03-22 18:13:44 +0100

    fix: set to nullable the workshop description field

--- database/migrations/2026_03_21_095158_create_workshops_table.php
+++ database/migrations/2026_03_21_095158_create_workshops_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
         Schema::create('workshops', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->text('description');
+            $table->text('description')->nullable();
             $table->dateTime('starts_at');
             $table->dateTime('ends_at');
             $table->unsignedInteger('capacity');
